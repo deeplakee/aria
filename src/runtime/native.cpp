66d36fc6def3fbab164ae87e32ef84aa241fad0c
icelake <1531978696@qq.com>
2025-11-04 23:16:32 +0800

    slove overflow in builtin random function

--- src/runtime/native.cpp
+++ src/runtime/native.cpp
@@ -20,7 +20,7 @@ Value Native::_aria_clock_(AriaEnv *env, int argCount, Value *args)
 Value Native::_aria_random_(AriaEnv *env, int argCount, Value *args)
 {
     static std::mt19937 gen(std::random_device{}());
-    static std::uniform_int_distribution<> dis(0, UINT32_MAX);
+    static std::uniform_int_distribution<> dis(std::mt19937::min(), std::mt19937::max());
 
     return number_val(dis(gen));
 }
